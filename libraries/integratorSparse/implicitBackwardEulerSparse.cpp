c7a4ac2d16f79ded5605f81ca99f9347b9d0e921
Bryn Lloyd <lloyd@itis.swiss>
2023-11-10 13:33:52 +0100

    add support for SPOOLES

--- libraries/integratorSparse/implicitBackwardEulerSparse.cpp
+++ libraries/integratorSparse/implicitBackwardEulerSparse.cpp
@@ -39,12 +39,7 @@
 #include "implicitBackwardEulerSparse.h"
 #include "integratorSolverSelection.h"
 
-#ifdef PARDISO
-  #include "sparseSolvers.h"
-#endif
-#ifdef PCG
-  #include "CGSolver.h"
-#endif
+#include "sparseSolvers.h"
 
 ImplicitBackwardEulerSparse::ImplicitBackwardEulerSparse(int r, double timestep, SparseMatrix * massMatrix_, ForceModel * forceModel_, int numConstrainedDOFs_, int * constrainedDOFs_, double dampingMassCoef, double dampingStiffnessCoef, int maxIterations, double epsilon, int numSolverThreads_): ImplicitNewmarkSparse(r, timestep, massMatrix_, forceModel_, numConstrainedDOFs_, constrainedDOFs_, dampingMassCoef, dampingStiffnessCoef, maxIterations, epsilon, 0.25, 0.5, numSolverThreads_)
 {
